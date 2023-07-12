074b24cd00698804e770c1e7fe0e6192468d64bf
bneradt <bneradt@yahooinc.com>
2023-07-12 16:21:34 -0500

    Updates for ssh-auth in docker containers

--- docker_context/aliases.zsh
+++ docker_context/aliases.zsh
@@ -23,3 +23,5 @@ alias urldecode='python3 -c "import sys, urllib.parse as ul; \
     print(ul.unquote_plus(sys.argv[1]))"'
 alias urlencode='python3 -c "import sys, urllib.parse as ul; \
     print (ul.quote_plus(sys.argv[1]))"'
+
+alias chmod_auth_sock='sudo chmod a+w /run/host-services/ssh-auth.sock'
